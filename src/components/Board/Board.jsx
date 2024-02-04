d770d433860cfdeab05dfeab71c5b3c8f55bfae4
Jasim Uddin <jasim.ice.iu@gmail.com>
2024-02-04 11:47:59 +0100

    state for board component added and props sent to square component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Square from '../Square/Square'
 
 const Board = () => {
+    const [squares, SetSquares] = useState(Array(9).fill(null));
+
+    const handleClick = () => {
+        console.log('clicked');
+    };
     return (
         <>
             <div className='flex'>
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[0]} onSquareClick={handleClick} />
+                <Square value={squares[1]} />
+                <Square value={squares[2]} />
             </div>
             <div className='flex'>
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[3]} />
+                <Square value={squares[4]} />
+                <Square value={squares[5]} />
             </div>
             <div className='flex'>
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[6]} />
+                <Square value={squares[7]} />
+                <Square value={squares[8]} />
             </div>
         </>
 
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import { useState } from 'react';
 
-const Square = () => {
+const Square = ({ value, onSquareClick }) => {
 
-    const [value, setValue] = useState(null);
-    const handleClicked = () => {
-        setValue('X');
-    }
 
     return (
         <>
-            <button className='bg-white border border-gray-600 h-12 w-12 m-1 leading-9 text-lg' onClick={handleClicked}>{value}</button>
+            <button className='bg-white border border-gray-600 h-12 w-12 m-1 leading-9 text-lg' onClick={onSquareClick}>{value}</button>
         </>
     )
 }
